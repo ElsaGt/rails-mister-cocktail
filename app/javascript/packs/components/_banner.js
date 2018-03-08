4fc8c592def58d4ca1d0b9eaee5aa3665153ab57
Elsa Gounot <elsa.gounot@gmail.com>
2018-03-08 23:34:29 +0100

    style of the index

---
+++ app/javascript/packs/components/_banner.js
@@ -0,0 +1,9 @@
+import Typed from 'typed.js';
+
+function loadDynamicBannerText() {
+  new Typed('#banner-typed-text', {
+    strings: ["What do you want to drink"],
+    typeSpeed: 50,
+    loop: true
+  });
+}
