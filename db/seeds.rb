b949604dc18d050f3cf209d4f899afd6542cc375
Elsa Gounot <elsa.gounot@gmail.com>
2018-03-09 14:53:43 +0100

    little style and cocktail ingredients API

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'open-uri'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,10 +6,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
-Ingredient.create(name: "strawberry")
-Ingredient.create(name: "vodka")
-Ingredient.create(name: "gin")
-Ingredient.create(name: "tonic")
+puts 'Cleaning database...'
+Dose.destroy_all
+Ingredient.destroy_all
+
+puts 'Creating ingredients...'
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+response = open(url).read
+json = JSON.parse(response)
+json["drinks"].each do |result|
+ ingredient_name = result["strIngredient1"]
+ Ingredient.create(name: ingredient_name)
+end
+
+puts 'Finished!'
